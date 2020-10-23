29270aabc014b952e068935612e9933c8d1b861e
Lohan Luiz <lohanluiz12@gmail.com>
2020-10-22 20:33:37 -0400

    useState concept

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Header from './components/Header';
 
 export default function App() {
-  const projects = ['Desenvolvimento de app', 'Front-end web'];
+  const [projects, setProjects] = useState(['Desenvolvimento de app', 'Front-end web']);
 
   function handleAddProject() {
-    projects.push(`Novo projeto ${Date.now()}`);
+    setProjects([...projects, `Novo projeto ${Date.now()}`]);
 
     console.log(projects);
   }
