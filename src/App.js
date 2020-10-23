02ef6e3edbe0b7c44f2dca698deaa1bb036b3b53
Lohan Luiz <lohanluiz12@gmail.com>
2020-10-22 20:07:42 -0400

    Props - Children

--- src/App.js
+++ src/App.js
@@ -5,8 +5,19 @@ import Header from './components/Header';
 export default function App() {
   return (
     <>
-    <Header title="GoStack"/>
-    <Header title="Projects"/>
+    <Header title="GoStack">
+      <ul>
+        <li>Homepage</li>
+        <li>Projects</li>
+      </ul>
+    </Header>
+    <Header title="Rocketseat">
+      <ul>
+        <li>Homepage</li>
+        <li>Projects</li>
+        <li>login</li>
+      </ul>
+    </Header>
     </>
   );
 }
